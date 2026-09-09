f09370717768eb34b9c85b0b7ec8316b8f642343
Steven Bold <stevenbold@hotmail.com>
2026-09-09 13:43:00 +0100

    pipeline_run_count: job vm-8k177.12.3 passed Built and Proven
    Job: vm-8k177.12.3

--- src/pipeline_run_count/test_pipeline_run_count.py
+++ src/pipeline_run_count/test_pipeline_run_count.py
@@ -1,5 +1,5 @@
 from pipeline_run_count.pipeline_run_count import pipeline_run_count
 
 
-def test_pipeline_run_count_returns_nine():
-    assert pipeline_run_count() == 9
+def test_pipeline_run_count_returns_ten():
+    assert pipeline_run_count() == 10
